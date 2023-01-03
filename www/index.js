f95382cd5ac0b2f5f9148552049ce153f6bd71f0
Tomoya Kashifuku <tnyo43@gmail.com>
2023-01-03 23:25:32 +0900

    convert image from web app

--- www/index.js
+++ www/index.js
@@ -1,3 +1,40 @@
-import * as wasm from "hello-wasm-pack";
+import { swap_color_channels } from '../pkg/image_process_app';
 
-wasm.greet();
+window.onload = () => {
+  const imageInput = document.getElementById('input-image');
+  console.log(imageInput);
+
+  imageInput.addEventListener('change', (event) => {
+    const image = new Image();
+    image.onload = () => {
+      const { width, height } = image;
+
+      const canvas = document.getElementById('original-canvas');
+      canvas.width = width;
+      canvas.height = height;
+      canvas.getContext('2d').drawImage(image, 0, 0, width, height);
+      const { data } = canvas
+        .getContext('2d')
+        .getImageData(0, 0, width, height);
+
+      const converted = swap_color_channels(data, width, height);
+      const convertedImage = new ImageData(
+        new Uint8ClampedArray(converted),
+        width
+      );
+      const convertedCanvas = document.getElementById('converted-canvas');
+      convertedCanvas.width = width;
+      convertedCanvas.height = height;
+      convertedCanvas
+        .getContext('2d')
+        .putImageData(convertedImage, 0, 0, 0, 0, width, height);
+    };
+
+    const reader = new FileReader();
+    reader.onload = () => {
+      image.src = reader.result;
+    };
+
+    reader.readAsDataURL(event.target.files[0]);
+  });
+};
